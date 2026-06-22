03a2e96886c3025665c2f4a450a857aacb151cbb
TOGURO Mikito <mit@shalab.net>
2026-06-22 13:19:05 +0900

    tagetver.h にヘッダーガードの追加

--- targetver.h
+++ targetver.h
@@ -1,6 +1,12 @@
 ﻿#pragma once
 
+#if !defined( TARGETVER_H_HEADER_GUARD )
+#define TARGETVER_H_HEADER_GUARD 1
+
 // // SDKDDKVer.h をインクルードすると、利用できる最も高いレベルの Windows プラットフォームが定義されます。
 // 以前の Windows プラットフォーム用にアプリケーションをビルドする場合は、WinSDKVer.h をインクルードし、
 // サポートしたいプラットフォームに _WIN32_WINNT マクロを設定してから SDKDDKVer.h をインクルードします。
 #include <SDKDDKVer.h>
+
+#endif /* !defined( TARGETVER_H_HEADER_GUARD ) */
+
